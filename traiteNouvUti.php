a6cd1c4b05ee7a0db05783262bd6cb8244bed3ae
BuildTools <unconfigured@null.spigotmc.org>
2021-03-30 22:30:15 +0200

    Near to the end

--- traiteNouvUti.php
+++ traiteNouvUti.php
@@ -1,43 +1,45 @@
 ﻿<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"><head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>FaceLOOK</title>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>FaceLOOK</title>
+    </head>
 
-</head>
+    <body>
+        <?php
+            include("FaceLookMenu.php");
+        ?>
 
-<body>
-<?php include("FaceLookMenu.php"); ?>
+        <div id="corps">
+            <?php
+                try {
+                    $connexion = new PDO('mysql:host=localhost;dbname=facelook','root', '');
+                } catch(PDOException $e) {
+                    print $e->getMessage() . "<br/>";
+                    die();
+                }
 
-<div id="corps">
-<?php
-	try {
-		$connexion = new PDO('mysql:host=localhost;dbname=facelook','root', '');
-	} catch (PDOException $e) {
-		print $e->getMessage() . "<br/>";
-		die();
-	}
+                if(!isset($_POST['nom']) || !isset($_POST['prenom']) || !isset($_POST['pseudo']) ||
+                    empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['pseudo'])) {
+                    echo '<p class="erreur"> Il manque des informations obligatoires </p>';
+                    echo '<p class="erreur">';
 
-if (!isset($_POST['nom']) ||!isset($_POST['prenom']) ||!isset($_POST['pseudo']) 
-	|| empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['pseudo']))
-	{
-		echo '<p class="erreur">Il manque des informations obligatoires</p>';
-		echo '<p class="erreur">';
-		if (!isset($_POST['nom'])|| empty($_POST['nom']))	echo 'Nom <br />';
-		if (!isset($_POST['prenom'])|| empty($_POST['prenom']))	echo 'Prenom <br />';
-		if (!isset($_POST['pseudo'])|| empty($_POST['pseudo']))	echo 'Pseudo <br />';
-		
-		echo '<a href="FaceLookNouvUtil.php">Retour</a></p>';
-	}else{
-		// requete d'insertion
-		$req = "INSERT INTO `Utilisateur` (`Pseudo`, `nom`, `prenom`) VALUES ('".$_POST['pseudo']."', '".$_POST['nom']."', '".$_POST['prenom']."');";
-		// execution de la requete et vérification d'erreur
-		if(! $connexion->query($req)){
-			echo " Problème : $req <br/> "; print_r($connexion->errorInfo());
-		}else{
-			echo 'Utilisateur ajouté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
-		}
-	}
-	$connexion = null; // deconnexion
-?>
-</div>
-</body>
+                    if(!isset($_POST['nom']) || empty($_POST['nom'])) echo 'Nom <br />';
+                    if(!isset($_POST['prenom']) || empty($_POST['prenom']))	echo 'Prenom <br />';
+                    if(!isset($_POST['pseudo']) || empty($_POST['pseudo']))	echo 'Pseudo <br />';
+
+                    echo '<a href="FaceLookNouvUtil.php"> Retour </a></p>';
+                } else {
+                    // requete d'insertion
+                    $req = "INSERT INTO `Utilisateur` (`Pseudo`, `nom`, `prenom`) VALUES ('".$_POST['pseudo']."', '".$_POST['nom']."', '".$_POST['prenom']."');";
+                    // execution de la requete et vérification d'erreur
+                    if(!$connexion -> query($req)) {
+                        echo " Problème : $req <br/> "; print_r($connexion -> errorInfo());
+                    } else {
+                        echo 'Utilisateur ajouté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
+                    }
+                }
+                $connexion = null; // deconnexion
+            ?>
+        </div>
+    </body>
 </html>
--- traiteUtil.php
+++ traiteUtil.php
@@ -1,54 +1,52 @@
-<?php session_start() ; ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"><head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>FaceLOOK</title>
+<?php session_start() ; ?>
 
-</head>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>FaceLOOK</title>
+    </head>
 
-<body>
-<?php include("FaceLookMenu.php"); ?>
+    <body>
+        <?php
+            include("FaceLookMenu.php");
+        ?>
 
-<div id="corps">
- <?php
- try {
- 	// 		creer une session 
- $connexion= new PDO("mysql:host=localhost;dbname=FaceLOOK","root", "");
-if ( isset ( $_POST[ 'Pseudo' ])) {
-$Pseudo = $_POST[ 'Pseudo' ];
+        <div id="corps">
+            <?php
+                try {
+             	    // creer une session
+                    $connexion= new PDO("mysql:host=localhost;dbname=FaceLOOK","root", "");
+
+                    if(isset($_POST['Pseudo'])) {
+                        $Pseudo = $_POST['Pseudo'];
 	
-$requete = 'SELECT Pseudo FROM utilisateur WHERE Pseudo LIKE "' . $Pseudo . '"' ;
-$reponse = $connexion -> query ( $requete );
+                        $requete = "SELECT Pseudo FROM utilisateur WHERE Pseudo LIKE '" . $Pseudo . "'";
+                        $reponse = $connexion -> query($requete);
 
+                        while($donnees = $reponse -> fetch()) {
+	                        // si l'utilisateur existe
 
-while ( $donnees = $reponse -> fetch ())
-{
-	// si l'utilisateur existe 
-	if ( $Pseudo == $donnees [ 'Pseudo' ] )
-		{
-			
-			$_SESSION['Utilisateur'] = $_POST[ 'Pseudo' ];
-			// 		redirection vers la page du mur (ou tout simplement donnée lien vers la page du mur.)
-			header ( 'Location: FaceLookMur.php' );
-			//echo 'Utilisateur connecté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
-		}
-		else
-		{
-		// 		si non  -> utilisateur inconnue 
-		// 			lien vers la page de connexion 
-			echo  'Pseudo incorrecte !! ' ;
-			 header ( 'Emplacement: FaceLookNouvUtil.php' );
-		}
+                            if($Pseudo == $donnees['Pseudo']) {
+            			        $_SESSION['Utilisateur'] = $_POST['Pseudo'];
+			                    //redirection vers la page du mur (ou tout simplement donnée lien vers la page du mur.)
+    			                header('Location: FaceLookMur.php');
+	    		                //echo 'Utilisateur connecté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
+		                    } else {
+		                        //si non  -> utilisateur inconnue
+		                        //lien vers la page de connexion
 
-}
-}
-} 
-catch (PDOException $e) {
-		print "Erreur !: " . $e->getMessage() . "<br/>";
-		die();
-	}
-	$connexion = null; // deconnexion
- ?>
+                                echo 'Pseudo incorrecte !!';
+//                                header('Emplacement: FaceLookNouvUtil.php');
+                            }
+                        }
+                    }
+                } catch(PDOException $e) {
+        		    print $e -> getMessage() . "<br/>";
+		            die();
+	            }
+        	    $connexion = null; // deconnexion
+            ?>
 
-</div>
-</body>
+        </div>
+    </body>
 </html>
