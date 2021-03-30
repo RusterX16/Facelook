2ac476b43f71b854c273cc8b677c9f023ddf2c81
BuildTools <unconfigured@null.spigotmc.org>
2021-03-31 01:35:06 +0200

    Near to the end

--- traiteUtil.php
+++ traiteUtil.php
@@ -17,27 +17,36 @@
              	    // creer une session
                     $connexion= new PDO("mysql:host=localhost;dbname=FaceLOOK","root", "");
 
+                    if(empty($_POST['Pseudo'])) {
+                        echo "<script> alert('Veuillez saisir le champ manquant !') </script>";
+                        echo "<script> window.location = 'FaceLookNouvUtil.php' </script>";
+                    }
+
                     if(isset($_POST['Pseudo'])) {
                         $Pseudo = $_POST['Pseudo'];
 	
                         $requete = "SELECT Pseudo FROM utilisateur WHERE Pseudo LIKE '" . $Pseudo . "'";
                         $reponse = $connexion -> query($requete);
 
-                        while($donnees = $reponse -> fetch()) {
+                        // J'ai remplacé le while par un if
+                        if($donnees = $reponse -> fetch()) {
 	                        // si l'utilisateur existe
 
                             if($Pseudo == $donnees['Pseudo']) {
-            			        $_SESSION['Utilisateur'] = $_POST['Pseudo'];
-			                    //redirection vers la page du mur (ou tout simplement donnée lien vers la page du mur.)
-    			                header('Location: FaceLookMur.php');
-	    		                //echo 'Utilisateur connecté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
-		                    } else {
-		                        //si non  -> utilisateur inconnue
-		                        //lien vers la page de connexion
+                                $_SESSION['Utilisateur'] = $_POST['Pseudo'];
+                                //redirection vers la page du mur (ou tout simplement donnée lien vers la page du mur.)
 
-                                echo 'Pseudo incorrecte !!';
-//                                header('Emplacement: FaceLookNouvUtil.php');
+                                header('Location: FaceLookMur.php');
+                                //echo 'Utilisateur connecté, vous pouvez aller sur <a href="FaceLookMur.php"> votre Mur</a>';
+		                    } else {
+                                //si non  -> utilisateur inconnue
+                                //lien vers la page de connexion
+
+//                              header('Emplacement: FaceLookNouvUtil.php');
                             }
+                        } else {
+                            echo "<script> alert('Le pseudo saisi est incorrect !') </script>";
+                            echo "<script> window.location = 'FaceLookNouvUtil.php' </script>";
                         }
                     }
                 } catch(PDOException $e) {
